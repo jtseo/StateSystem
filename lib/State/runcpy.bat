d6ab15f46cef72e0fc7f0d04002644278791573f
Seo Jung Tae <oj@oddeyesoft.com>
2024-01-31 23:38:30 +0900

    ignore state

--- lib/State/runcpy.bat
+++
@@ -1,8 +0,0 @@
-del /Q ..\PrototypeSii\Assets\StreamingAssets\State\*.*
-del /Q ..\PrototypeSii\Assets\VLogger\Resources\*.*
-copy *.data ..\PrototypeSii\Assets\StreamingAssets\State
-del ..\PrototypeSii\Assets\StreamingAssets\State\*Pos.data
-copy *.define ..\PrototypeSii\Assets\StreamingAssets\State
-copy ..\PrototypeSii\Assets\StreamingAssets\State\*.* ..\PrototypeSii\Assets\VLogger\Resources
-ren ..\PrototypeSii\Assets\VLogger\Resources\*.data *.data.bytes
-ren ..\PrototypeSii\Assets\VLogger\Resources\*.define *.define.bytes
