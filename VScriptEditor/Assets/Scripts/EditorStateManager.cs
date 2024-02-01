f4350bdd6afed7022e7ce9aefdf74e9dbd59a44d
jtseo_archipin <jungtae.seo@archipin.com>
2024-02-01 18:50:47 +0900

    vs build

---
+++ VScriptEditor/Assets/Scripts/EditorStateManager.cs
@@ -0,0 +1,19 @@
+﻿namespace StateSystem
+{
+    public class EditorStateManager : VLStateManager
+    {
+        public override void StateFuncRegistor()
+        {
+            base.StateFuncRegistor();
+            
+            UxViewMain.StateFuncRegist();
+            UxViewStateContent.StateFuncRegist();
+            UxGroupID.StateFuncRegist();
+            VScriptLink.StateFuncRegist();
+            UxViewColumn.StateFuncRegist();
+            UxViewColumnEditor.StateFuncRegist();
+            //VScriptCosmosMenu.StateFuncRegist();
+            VScriptLogHistory.StateFuncRegist();
+        }
+    }
+}
