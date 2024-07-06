96cdb591455bec0f4fddeaf18f078ea02f74871c
Seo Jung Tae <oj@oddeyesoft.com>
2024-07-07 00:43:06 +0900

    mac build

--- lib/BaseLibrary/ApStateSystem/StateManagerPnID.cpp
+++ lib/BaseLibrary/ApStateSystem/StateManagerPnID.cpp
@@ -62,7 +62,9 @@ void StateManagerPnID::release_manager(bool _reset)
 //	BaseDStructure::set_callback_fileclose(fnCallbackFileClose);
 //}
 
+#ifdef WIN32
 #include <winsock2.h>
+#endif
 #include "../PtBase/BaseSFuncDirectory.h"
 #include "../PtBase/BaseTextEditor.h"
 #include "../PtBase/BaseTextEditorText.h"
@@ -95,7 +97,10 @@ StateManagerPnID::StateManagerPnID(void *_applet) :BaseStateManager(_applet)
 	ExtendOpenCV extOpencv;
 	BaseSCTcp tcp;
 	//BaseSFParser paser;
+#ifdef WIN32
 	BaseStateSystem sys;
+#endif
+	
 #ifdef PHOTOSKETCH
 	PhotoSketch	sketch;
 #else
